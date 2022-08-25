0194433bc0478251c81ed6b90288d33681cd9692
MehakCoder <cmehak26@gmail.com>
2022-08-25 12:20:30 +0530

    add image and modify the index and java page

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ function closeSearch() {
   document.getElementById("side-search-open").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 } 
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
