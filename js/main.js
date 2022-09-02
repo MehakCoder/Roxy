8ad2cb1b36a1bb861e2950d07721bbc1bfa46daa
MehakCoder <cmehak26@gmail.com>
2022-09-02 14:54:30 +0530

    add about and modify the mainjs

--- js/main.js
+++ js/main.js
@@ -71,4 +71,34 @@ $(function () {
   AOS.init();
 
 });
+//7. progress bar
+const skillsSection = document.getElementById('skill-item');
 
+const progressBars = document.querySelectorAll('.progress-bar');
+
+function  showProgress(){
+   progressBars.forEach(progressBar=>{
+        const value = progressBar.dataPercent.progress;
+        progressBar.style.opacity = 1;
+        progressBar.style.width='${value}%';  
+   });
+}
+function hideProgress(){
+  progressBars.forEach(p => {
+    p.style.opacity = 0;
+    p.style.width = 0;
+  });
+}
+
+window.addEventListener('scroll',() =>{
+  const sectionPos = skillsSection.getBoundingClientRect().top;
+  const screenPos  = window.innerHeight;
+
+  if(sectionPos < screenPos){
+    showProgress();
+  }else{
+    hideProgress();
+  } 
+
+  }
+);
